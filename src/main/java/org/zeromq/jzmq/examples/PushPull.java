fba33eb4e002040d8cad9c3e713ed733bde80551
Trevor Bernard <trevor.bernard@gmail.com>
2013-02-07 18:02:33 -0400

    Update PushPull example

--- src/main/java/org/zeromq/jzmq/examples/PushPull.java
+++ src/main/java/org/zeromq/jzmq/examples/PushPull.java
@@ -1,13 +1,13 @@
 package org.zeromq.jzmq.examples;
 
+import org.zeromq.ContextFactory;
 import org.zeromq.api.Context;
 import org.zeromq.api.Socket;
 import org.zeromq.api.SocketType;
-import org.zeromq.jzmq.ManagedContext;
 
 public class PushPull {
     public static void main(String[] args) throws Exception {
-        Context ctx = new ManagedContext(1); // This should be some sort of factory to avoid using concrete classes
+        Context ctx = ContextFactory.createContext(1);
         Socket puller = ctx.createSocket(SocketType.PULL).bind("ipc:///tmp/pushpull.ipc");
         Socket pusher = ctx.createSocket(SocketType.PUSH).connect("ipc:///tmp/pushpull.ipc");
         pusher.send("PING".getBytes());
