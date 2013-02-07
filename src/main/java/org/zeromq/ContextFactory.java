5bc6ba15960ce169a14c586d7be29b40ba5b0d9e
Trevor Bernard <trevor.bernard@gmail.com>
2013-02-07 16:30:35 -0400

    Add ContextFactory to avoid using concrete classes

---
+++ src/main/java/org/zeromq/ContextFactory.java
@@ -0,0 +1,17 @@
+package org.zeromq;
+
+import org.zeromq.ZMQ;
+import org.zeromq.api.Context;
+import org.zeromq.jzmq.ManagedContext;
+
+public class ContextFactory {
+    private ContextFactory() {
+    }
+
+    public static Context createContext(int ioThreads) {
+        if(ioThreads < 0) {
+            throw new IllegalArgumentException("ioThreads must be positive");
+        }
+        return new ManagedContext(ZMQ.context(ioThreads));
+    }
+}
