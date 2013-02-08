66e478a065044b38916f6c3ef1c90cce9678fbea
Trevor Bernard <trevor.bernard@gmail.com>
2013-02-07 20:50:08 -0400

    Add slf4j-api log support
    Require logback-classic as provided and put logback.xml in src/test/resources
    so we can use it but not force our users to be bound to it.

--- src/main/java/org/zeromq/jzmq/ManagedContext.java
+++ src/main/java/org/zeromq/jzmq/ManagedContext.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zeromq.ZMQ;
 import org.zeromq.api.Context;
 import org.zeromq.api.Socket;
@@ -20,6 +22,8 @@ import org.zeromq.jzmq.sockets.SocketBuilder;
  * Manage JZMQ Context
  */
 public class ManagedContext implements Context, Closeable {
+    private static final Logger log = LoggerFactory.getLogger(ManagedContext.class);
+
     private final AtomicBoolean closed = new AtomicBoolean(false);
 
     private final ZMQ.Context context;
@@ -52,7 +56,7 @@ public class ManagedContext implements Context, Closeable {
                 socket.getZMQSocket().close();
             } catch (Exception ignore) {
             }
-            System.out.println("Destroyed socket");
+            log.info("closed socket");
             sockets.remove(socket);
         }
     }
@@ -65,10 +69,12 @@ public class ManagedContext implements Context, Closeable {
             }
             sockets.clear();
             context.term();
-            System.out.println("Killed context");
+            log.info("closed context");
         }
     }
 
+    // I'd really like this to be private but I don't want all the builders in here
+    // If we only deal with the Socket interface, callers won't see this
     public void addSocket(Socket socket) {
         sockets.add(socket);
     }
