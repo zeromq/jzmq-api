5d781b1e15f27884372ca515284c321c46ea542b
Trevor Bernard <trevor.bernard@gmail.com>
2013-02-09 15:17:19 -0400

    Add javadocs to DeviceType

--- src/main/java/org/zeromq/api/DeviceType.java
+++ src/main/java/org/zeromq/api/DeviceType.java
@@ -1,20 +1,44 @@
 package org.zeromq.api;
 
-import org.zeromq.ZMQ;
-
 /**
- * Values for device types
+ * Values for Device types
  */
 public enum DeviceType {
-    STREAMER(ZMQ.STREAMER), FORWARDER(ZMQ.FORWARDER), QUEUE(ZMQ.QUEUE);
-
-    private final int type;
-
-    DeviceType(int type) {
-        this.type = type;
-    }
-
-    public int getType() {
-        return type;
-    }
+    /**
+     * ZMQ_STREAMER collects tasks from a set of pushers and forwards these to a set of pullers. You will generally use
+     * this to bridge networks. Messages are fair-queued from pushers and load-balanced to pullers.
+     */
+    STREAMER {
+        @Override
+        public int getType() {
+            return 1;
+        }
+    },
+    /**
+     * ZMQ_FORWARDER collects messages from a set of publishers and forwards these to a set of subscribers. You will
+     * generally use this to bridge networks, e.g. read on TCP unicast and forward on multicast.
+     */
+    FORWARDER {
+        @Override
+        public int getType() {
+            return 2;
+        }
+    },
+    /**
+     * ZMQ_QUEUE creates a shared queue that collects requests from a set of clients, and distributes these fairly among
+     * a set of services. Requests are fair-queued from frontend connections and load-balanced between backend
+     * connections. Replies automatically return to the client that made the original request.
+     */
+    QUEUE {
+        @Override
+        public int getType() {
+            return 3;
+        }
+    };
+    /**
+     * Device type connects a frontend socket to a backend socket.
+     * 
+     * @return device type
+     */
+    public abstract int getType();
 }
