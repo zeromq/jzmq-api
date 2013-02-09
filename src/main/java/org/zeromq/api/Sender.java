cfabb3fe0f47f2946d8f4be5a409bf089ad654c3
Trevor Bernard <trevor.bernard@gmail.com>
2013-02-09 18:36:49 -0400

    Add javadoc comments to Sender

--- src/main/java/org/zeromq/api/Sender.java
+++ src/main/java/org/zeromq/api/Sender.java
@@ -1,8 +1,22 @@
 package org.zeromq.api;
 
-
+/**
+ * 
+ */
 public interface Sender {
+    /**
+     * Insert the specified element into the ØMQ Socket queue.
+     * 
+     * @param buf element
+     */
     public void send(byte[] buf);
 
+    /**
+     * Insert the specified element into the ØMQ Socket queue.
+     * 
+     * @param buf
+     * @param offset
+     * @param flag send flag
+     */
     public void send(byte[] buf, int offset, MessageFlag flag);
 }
