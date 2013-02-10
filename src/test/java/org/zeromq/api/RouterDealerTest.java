0b58fa9be01388cf39038cf2cbbea5d8fcf68d79
Trevor Bernard <trevor.bernard@gmail.com>
2013-02-10 14:35:33 -0400

    Add DealerToRouter test

--- src/test/java/org/zeromq/api/RouterDealerTest.java
+++ src/test/java/org/zeromq/api/RouterDealerTest.java
@@ -21,23 +21,44 @@ public class RouterDealerTest extends TestCase {
     }
 
     @Test(timeout = 1000)
-    public void testSimpleRouterDealer() throws Exception {
+    public void testRoutingToCorrectDealerFromRouter() throws Exception {
         Socket router = context.buildSocket(SocketType.ROUTER).bind("ipc://router.ipc");
-        Socket dealer1 = context.buildSocket(SocketType.DEALER).withIdentity("A".getBytes()).connect("ipc://router.ipc");
-        Socket dealer2 = context.buildSocket(SocketType.DEALER).withIdentity("B".getBytes()).connect("ipc://router.ipc");
+        Socket dealer1 = context.buildSocket(SocketType.DEALER).withIdentity("A".getBytes())
+                .connect("ipc://router.ipc");
+        Socket dealer2 = context.buildSocket(SocketType.DEALER).withIdentity("B".getBytes())
+                .connect("ipc://router.ipc");
 
         // Unfortunately you have to wait until the dealers connect or dealer1.receive will block. Add poll later
         Thread.sleep(100);
         router.send("A".getBytes(), 0, MessageFlag.SEND_MORE);
         router.send("END".getBytes());
-        
+
         String expected = new String(dealer1.receive());
         assertEquals("END", expected);
 
         router.send("B".getBytes(), 0, MessageFlag.SEND_MORE);
         router.send("END".getBytes());
-        
+
         expected = new String(dealer2.receive());
         assertEquals("END", expected);
+    }
+
+    @Test(timeout = 1000)
+    public void testDealerToRouter() throws Exception {
+        Socket router = context.buildSocket(SocketType.ROUTER).bind("ipc://router.ipc");
+        Socket dealer = context.buildSocket(SocketType.DEALER).withIdentity("A".getBytes()).connect("ipc://router.ipc");
+
+        dealer.send("PING".getBytes());
+        byte[] id = router.receive();
+        byte[] expected = router.receive();
+
+        assertEquals("A", new String(id));
+        assertEquals("PING", new String(expected));
+
+        router.send(id, 0, MessageFlag.SEND_MORE);
+        router.send("END".getBytes());
+
+        expected = dealer.receive();
+        assertEquals("END", new String(expected));
     }
 }
