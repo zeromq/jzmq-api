cad9a4ded1351075eeea34205786a817f984e62a
John Watson <git@sleazyweasel.com>
2013-02-10 18:28:22 -0800

    printlns for debugging the broken build

--- src/test/java/org/zeromq/api/DealerRepTest.java
+++ src/test/java/org/zeromq/api/DealerRepTest.java
@@ -23,19 +23,31 @@ public class DealerRepTest extends TestCase {
 
     @Test(timeout = 1000)
     public void testDealerRep() throws Exception {
+        System.out.println("beginning of test");
         Socket dealerSocket = context.buildSocket(SocketType.DEALER).bind("inproc://serverSocket");
+        System.out.println("built dealer socket");
         Socket replySocket = context.buildSocket(SocketType.REP).connect("inproc://serverSocket");
+        System.out.println("built REP socket");
 
         dealerSocket.send("returnAddress".getBytes(), MessageFlag.SEND_MORE);
+        System.out.println("sent address");
         dealerSocket.send("".getBytes(), MessageFlag.SEND_MORE);
+        System.out.println("sent blank");
         dealerSocket.send("request".getBytes());
+        System.out.println("sent request frame");
 
         byte[] request = replySocket.receive();
+        System.out.println("request received");
         assertArrayEquals("request".getBytes(), request);
+        System.out.println("asserted array equal");
         replySocket.send("response".getBytes());
+        System.out.println("sent response");
 
         assertArrayEquals("returnAddress".getBytes(), dealerSocket.receive());
+        System.out.println("asserted return address");
         assertArrayEquals("".getBytes(), dealerSocket.receive());
+        System.out.println("asserted blank");
         assertArrayEquals("response".getBytes(), dealerSocket.receive());
+        System.out.println("asserted response");
     }
 }
