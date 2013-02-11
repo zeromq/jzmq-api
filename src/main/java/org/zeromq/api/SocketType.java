d40f2c0a22f6a7da59ef7eae0ae618655c9bfbd8
Trevor Bernard <trevor.bernard@gmail.com>
2013-02-11 11:29:39 -0400

    Revert SocketType to it's previous version
    The reason is because we intend on becoming the API for more than one implementation of ZMTP.

--- src/main/java/org/zeromq/api/SocketType.java
+++ src/main/java/org/zeromq/api/SocketType.java
@@ -1,7 +1,5 @@
 package org.zeromq.api;
 
-import org.zeromq.ZMQ;
-
 /**
  * Values for Socket types
  */
@@ -10,56 +8,56 @@ public enum SocketType {
      * A socket of type ZMQ_PAIR can only be connected to a single peer at any one time. No message routing or filtering
      * is performed on messages sent over a ZMQ_PAIR socket.
      */
-    PAIR(ZMQ.PAIR),
+    PAIR(0),
     /**
      * A socket of type ZMQ_PUB is used by a publisher to distribute data. Messages sent are distributed in a fan out
      * fashion to all connected peers.
      */
-    PUB(ZMQ.PUB),
+    PUB(1),
     /**
      * A socket of type ZMQ_SUB is used by a subscriber to subscribe to data distributed by a publisher. Initially a
      * ZMQ_SUB socket is not subscribed to any messages.
      */
-    SUB(ZMQ.SUB),
+    SUB(2),
     /**
      * A socket of type ZMQ_REP is used by a service to receive requests from and send replies to a client. This socket
      * type allows only an alternating sequence of receive(request) and subsequent send(reply) calls.
      */
-    REQ(ZMQ.REQ),
+    REQ(3),
     /**
      * A socket of type ZMQ_REQ is used by a client to send requests to and receive replies from a service. This socket
      * type allows only an alternating sequence of send(request) and subsequent receive(reply) calls.
      */
-    REP(ZMQ.REP),
+    REP(4),
     /**
      * A socket of type ZMQ_DEALER is an advanced pattern used for extending request/reply sockets. Each message sent is
      * round-robined among all connected peers, and each message received is fair-queued from all connected peers.
      */
-    DEALER(ZMQ.DEALER),
+    DEALER(5),
     /**
      * A socket of type ZMQ_ROUTER is an advanced socket type used for extending request/reply sockets. When receiving
      * messages a ZMQ_ROUTER socket shall prepend a message part containing the identity of the originating peer to the
      * message before passing it to the application.
      */
-    ROUTER(ZMQ.ROUTER),
+    ROUTER(6),
     /**
      * A socket of type ZMQ_PULL is used by a pipeline node to receive messages from upstream pipeline nodes. Messages
      * are fair-queued from among all connected upstream nodes.
      */
-    PULL(ZMQ.PULL),
+    PULL(7),
     /**
      * A socket of type ZMQ_PUSH is used by a pipeline node to send messages to downstream pipeline nodes. Messages are
      * round-robined to all connected downstream nodes.
      */
-    PUSH(ZMQ.PUSH),
+    PUSH(8),
     /**
      * Same as ZMQ_PUB except that you can receive subscriptions from the peers in form of incoming messages.
      */
-    XPUB(ZMQ.XPUB),
+    XPUB(9),
     /**
      * Same as ZMQ_SUB except that you subscribe by sending subscription messages to the socket.
      */
-    XSUB(ZMQ.XSUB);
+    XSUB(10);
 
     private final int type;
 
@@ -69,7 +67,7 @@ public enum SocketType {
 
     /**
      * Socket type which determines the semantics of communication over the socket.
-     *
+     * 
      * @return socket type
      */
     public int getType() {
