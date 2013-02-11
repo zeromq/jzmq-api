8a6e193eab9bb6b79847f0dc11d1fa96285a2e8c
John Watson <git@sleazyweasel.com>
2013-02-10 18:20:54 -0800

    stub for the msgqueue guide example. on hold until we have zmq_proxy.

---
+++ src/test/java/guide/msgqueue.java
@@ -0,0 +1,22 @@
+package guide;
+
+import org.zeromq.api.Socket;
+import org.zeromq.api.SocketType;
+import org.zeromq.jzmq.ManagedContext;
+
+import java.io.IOException;
+
+/**
+ * On hold until the zmq_proxy function is available in our version of the jzmq jni libs.
+ */
+public class msgqueue {
+    public static void main(String[] args) throws IOException {
+        ManagedContext context = new ManagedContext();
+
+        Socket frontEnd = context.buildSocket(SocketType.ROUTER).bind("tcp://*:5559");
+        Socket backEnd = context.buildSocket(SocketType.DEALER).bind("tcp://*:5560");
+
+//        context.proxy(frontEnd, backEnd);
+        context.close();
+    }
+}
