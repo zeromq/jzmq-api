ef4c99cec2834a88b7000d389fcb9d194a266368
Stephen Riesenberg <sjohnr@hotmail.com>
2015-05-13 22:46:53 -0500

    Changes to ZMQExceptions to support jeromq
    TODO: Change jzmq exception handling, e.g. return null like jeromq.

--- src/main/java/org/zeromq/api/Receiver.java
+++ src/main/java/org/zeromq/api/Receiver.java
@@ -3,55 +3,61 @@ package org.zeromq.api;
 import java.nio.ByteBuffer;
 
 /**
- * 
+ * Socket capable of receiving message parts.
  */
 public interface Receiver {
     /**
-     * Receive a message part from a socket
+     * Receive a message part from a socket.
      * 
-     * @return bytes
+     * @return The message bytes
      */
     byte[] receive();
 
     /**
-     * Receive a message part from a socket
+     * Receive a message part from a socket.
      * 
-     * @param flag message receive flag
-     * @return bytes
+     * @param flag Flag controlling behavior of the receive operation
+     * @return The message bytes
      */
     byte[] receive(MessageFlag flag);
 
     /**
+     * Receive a message part from a socket.
      * 
-     * @param buf
-     * @param offset
-     * @param len
-     * @param flag
-     * @return
+     * @param buf The byte buffer
+     * @param offset The buffer offset
+     * @param length The buffer length
+     * @param flag Flag controlling behavior of the receive operation
+     * @return The number of bytes read, -1 on error
      */
     int receive(byte[] buf, int offset, int len, MessageFlag flag);
 
     /**
-     * Receive a zero copy message part from a socket
+     * Receive a message part from a socket into a byte buffer.
      * 
-     * @param buf
-     * @param len
-     * @param flag
-     * @return the number of bytes received
+     * @param buf The byte buffer
+     * @param flag Flag controlling behavior of the receive operation
+     * @return The number of bytes read, -1 on error
      */
-    int receiveZeroCopy(ByteBuffer buf, int len, MessageFlag flag);
+    int receiveByteBuffer(ByteBuffer buf, MessageFlag flag);
 
     /**
      * This receiver has more of a multi-part message waiting for receipt.
+     * 
+     * @return true if there is more data, false otherwise
      */
     boolean hasMoreToReceive();
 
     /**
+     * Receive the full message (all frames) from the socket.
+     * 
      * @return The full message (all frames) from the socket.
      */
     Message receiveMessage();
 
     /**
+     * Receive a routed message (all frames) from the socket.
+     * 
      * @return The full message (all frames) from the socket, assuming it has been through a Router socket, and has
      * routing frames associated with it.
      */
--- src/main/java/org/zeromq/api/Sender.java
+++ src/main/java/org/zeromq/api/Sender.java
@@ -3,16 +3,51 @@ package org.zeromq.api;
 import java.nio.ByteBuffer;
 
 /**
- * 
+ * Socket capable of sending message parts.
  */
 public interface Sender {
+    /**
+     * Send a message part on a socket.
+     * 
+     * @param buf The message bytes
+     * @return true if the operation was successful, false otherwise
+     */
     boolean send(byte[] buf);
 
+    /**
+     * Send a message part on a socket.
+     * 
+     * @param buf The message bytes
+     * @param flag Flag controlling behavior of the send operation
+     * @return true if the operation was successful, false otherwise
+     */
     boolean send(byte[] buf, MessageFlag flag);
 
+    /**
+     * Send a message part on a socket.
+     * 
+     * @param buf The message bytes
+     * @param offset The buffer offset
+     * @param length The buffer length
+     * @param flag Flag controlling behavior of the send operation
+     * @return true if the operation was successful, false otherwise
+     */
     boolean send(byte[] buf, int offset, int length, MessageFlag flag);
 
-    boolean sendZeroCopy(ByteBuffer buf, int length, MessageFlag flag);
+    /**
+     * Send a message part on a socket from a byte buffer.
+     * 
+     * @param buf The byte buffer
+     * @param flag Flag controlling behavior of the send operation
+     * @return true if the operation was successful, false otherwise
+     */
+    boolean sendByteBuffer(ByteBuffer buf, MessageFlag flag);
 
+    /**
+     * Send the full message (all frames) on the socket.
+     * 
+     * @param message The full message
+     * @return true if the operation was successful, false otherwise
+     */
     boolean send(Message message);
 }
